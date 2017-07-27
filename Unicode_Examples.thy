5513fc70f6d15bb12decff87880262390edc8933
Lars Hupel <lars.hupel@mytum.de>
2017-07-27 11:10:53 +0200

    update to Isabelle2016-1

--- Unicode_Examples.thy
+++ Unicode_Examples.thy
@@ -2,6 +2,9 @@ theory Unicode_Examples
 imports Unicode
 begin
 
+term "ustr \<open>\\\<close>"
+
+term "ustr \<open>\011\<close>"
 term "ustr \<open>abcß\<close>"
 term "Ustring [[50], [70], [80], [195, 159]]"
 
