91962665c9c87143ee2fc4f8215db70019eaa642
Lars Hupel <lars.hupel@mytum.de>
2018-03-22 12:16:50 +0100

    tuned

--- Typed_Evaluation_Examples.thy
+++ Typed_Evaluation_Examples.thy
@@ -6,20 +6,31 @@ typed_evaluation data = \<open>int\<close>
 
 ML\<open>val x = 1\<close>
 
-ML\<open>Typed_Evaluation.eval_exn @{token data} \<open>1 + error "abc"\<close> @{context}\<close>
-ML\<open>Typed_Evaluation.eval @{token data} \<open>1 + error "abc"\<close> @{context}\<close>
-ML\<open>Typed_Evaluation.eval @{token data} \<open>1 + x\<close> @{context}\<close>
+ML\<open>
+  Typed_Evaluation.eval_exn @{token data} \<open>1 + error "abc"\<close> @{context};
+
+  val res = Typed_Evaluation.eval @{token data} \<open>1 + x\<close> @{context};
+  @{assert} (res = 2);
+
+  val res = Exn.capture (Typed_Evaluation.eval @{token data} \<open>1 + error "abc"\<close>) @{context};
+  @{assert} (is_some (Exn.get_exn res))
+\<close>
 
 ML\<open>@{token data}\<close>
 
 locale f
 begin
 
-  typed_evaluation data = \<open>int list\<close>
+typed_evaluation data = \<open>int list\<close>
 
-  ML\<open>Typed_Evaluation.eval @{token data} \<open>[1]\<close> @{context}\<close>
-  ML\<open>Typed_Evaluation.eval @{token Typed_Evaluation_Examples.data} \<open>[1]\<close> @{context}\<close>
-  ML\<open>Typed_Evaluation.eval @{token Typed_Evaluation_Examples.data} \<open>1\<close> @{context}\<close>
+ML\<open>
+  Typed_Evaluation.eval @{token data} \<open>[1]\<close> @{context};
+
+  Typed_Evaluation.eval @{token Typed_Evaluation_Examples.data} \<open>1\<close> @{context};
+
+  val res = Exn.capture (Typed_Evaluation.eval @{token Typed_Evaluation_Examples.data} \<open>[1]\<close>) @{context};
+  @{assert} (is_some (Exn.get_exn res))
+\<close>
 
 end
 
@@ -28,6 +39,5 @@ ML_val \<open>@{token "data"}\<close>
 end
 
 ML\<open>Typed_Evaluation.eval @{token data} \<open>1\<close> @{context}\<close>
-
 
 end
